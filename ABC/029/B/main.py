b0d6bd0a1e9a69a64e2aad23a90ace9cc30c2ceb
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-02-12 02:24:58 +0900

    2/12

---
+++ ABC/029/B/main.py
@@ -0,0 +1,1 @@
+print(len([l for l in [input() for i in range(12)] if 'r' in l]))
---
+++ halfMarathon/A/main.py
@@ -0,0 +1,5 @@
+n, k, h, w, t = map(int, input().split())
+[[ list(map(int, input())) for i in range(h)] for i in range(n)]
+# kが使用する数，tコマンド数
+
+
